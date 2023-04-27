6819c1128ab20b93c7b4c27ea91f93a0680a09f6
Dexdesigns <40634954@live.napier.ac.uk>
2023-04-27 21:36:18 +0300

    Nigerian Food Recipes
    Nigerian Food Recipes

---
+++ Nigerian Food Recipes/script.js
@@ -0,0 +1,30 @@
+
+// Mobile Menu
+
+let menu = document.querySelector ('.bx-menu');
+let navbar = document.querySelector ('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-window-close');
+    navbar.classList.toggle('open');
+}
+
+// Search bar
+
+document.querySelector('#search-input').addEventListener('input', filterList);
+
+function filterList() {
+  const searchInput = document.querySelector('#search-input');
+  const filter = searchInput.value.toLowerCase();
+  const listItems = document.querySelectorAll('.list-group-item');
+  
+  listItems.forEach((item) => {
+    let text = item.textContent.toLowerCase();
+    if (text.includes(filter)) {
+      item.style.display = '';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
